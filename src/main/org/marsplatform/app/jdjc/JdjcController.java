58d0b9bb1595fc08f626094a7cad16e7cda3215b
moxi-moxi <moxi-moxi@hotmail.com>
2017-03-27 23:46:58 +0800

    add echarts demo

---
+++ src/main/org/marsplatform/app/jdjc/JdjcController.java
@@ -0,0 +1,29 @@
+package org.marsplatform.app.jdjc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+/**
+ * 项目基本信息
+ * 
+ * @author mox
+ * @email moxi-moxi@hotmail.com
+ * @date 2017-03-27 18:32:22
+ */
+@Controller
+@RequestMapping("/jdjc")
+public class JdjcController {
+	
+	@RequestMapping("/chart.html")
+	public String chart(){
+		return "app/jdjc/chart.html";
+	}
+	
+	@RequestMapping("/map.html")
+	public String map(){
+		return "app/jdjc/map.html";
+	}
+
+	
+}
