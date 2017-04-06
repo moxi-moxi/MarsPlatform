a307f76af70c6bafa26b0f89c8a3505a852f7c3c
moxi-moxi <moxi-moxi@hotmail.com>
2017-04-06 23:13:54 +0800

    dev model

--- src/main/org/marsplatform/extend/system/web/SysLoginController.java
+++ src/main/org/marsplatform/extend/system/web/SysLoginController.java
@@ -64,11 +64,12 @@ public class SysLoginController {
 	@ResponseBody
 	@RequestMapping(value = "/sys/login", method = RequestMethod.POST)
 	public Result login(String username, String password, String captcha)throws IOException {
+		/*
 		String kaptcha = ShiroUtils.getKaptcha(Constants.KAPTCHA_SESSION_KEY);
 		if(!captcha.equalsIgnoreCase(kaptcha)){
 			return Result.error("验证码不正确");
 		}
-		
+		*/
 		try{
 			Subject subject = ShiroUtils.getSubject();
 			//sha256加密
